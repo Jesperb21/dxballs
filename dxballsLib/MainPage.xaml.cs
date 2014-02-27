f3268f04d30ae48786893873cbec70dd0b15ec7a
Jesperb21 <Jesper.j.b.baunsgaard@gmail.com>
2014-02-27 12:05:45 +0100

    asdfg u needed it

--- dxballsLib/MainPage.xaml.cs
+++ dxballsLib/MainPage.xaml.cs
@@ -34,7 +34,7 @@ namespace dxballsLib
         }
 
         public List<ContentControl> ballList = new List<ContentControl>();
-
+        private List<Point>oldBallCords = new List<Point>();
         public virtual object amountOfBalls
         {
             get;
@@ -81,8 +81,10 @@ namespace dxballsLib
             ballList.Add(new ContentControl() { Template = Resources["ballTemplate"] as ControlTemplate });
             //ballList[0].Template = Resources["ballTemplate"] as ControlTemplate;
             playArea.Children.Add(ballList[0]);
-            int xPos = 500;
-            ballList[0].Content = "Canvas.Left=" + xPos;
+            Canvas.SetLeft(ballList[0], 500);
+            Canvas.SetTop(ballList[0], 10);
+//            oldBallCords.Add(new Point(500, 0));
+            
         }
 
         public virtual void addEnemyLine()
@@ -97,7 +99,32 @@ namespace dxballsLib
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            Canvas.SetLeft(ballList[0], Canvas.GetLeft(ballList[0]) + 50);
+            bool i = true;
+            int speed = 10;
+            double nextRelativeXPos = new double();
+            double nextRelativeYPos = new double();
+            if (oldBallCords.Count >= 1)
+            {
+            double ydifference = oldBallCords[0].Y - Canvas.GetLeft(ballList[0]);
+            double xdifference = oldBallCords[0].X - Canvas.GetTop(ballList[0]);
+            double angle = Math.Atan(ydifference / xdifference);
+            nextRelativeXPos = Math.Cos(angle) * speed;
+            nextRelativeYPos = Math.Sin(angle) * speed;
+            i = false;
+            }
+            if (!i)
+            {
+                oldBallCords[0] = new Point(Canvas.GetLeft(ballList[0]), Canvas.GetTop(ballList[0]));
+            }
+            else
+            {
+                oldBallCords.Add(new Point(Canvas.GetLeft(ballList[0]), Canvas.GetTop(ballList[0])));
+            }
+            if (!i)
+            {
+                Canvas.SetLeft(ballList[0], Canvas.GetLeft(ballList[0]) + nextRelativeXPos);
+                Canvas.SetTop(ballList[0], Canvas.GetLeft(ballList[0]) + nextRelativeYPos);
+            }
         }
     }
 }
