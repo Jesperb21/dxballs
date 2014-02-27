b5e7784897ecc7720714a48f7dd566113b396e46
Jesperb21 <Jesper.j.b.baunsgaard@gmail.com>
2014-02-27 19:55:32 +0100

    player movements virker, ball movements startet på, tilføjet et main loop, tilføjet 2 controltemplates, "playerTemplate" & "ballTemplate", tilføjet en start game knap. tilføjet et canvas med x:name "playArea". fjernet x:name fra grid'et0
    kodefiler ændret)
    	MainWindow.xaml.cs

--- dxballslib/MainWindow.xaml.cs
+++ dxballslib/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Timers;
+using System.Windows.Threading;
 
 namespace dxballslib
 {
@@ -21,10 +22,56 @@ namespace dxballslib
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        private DispatcherTimer mainLoop = new DispatcherTimer();
+        private ContentControl playerBlock = new ContentControl();
+        private ContentControl ball1 = new ContentControl();
         public MainWindow()
         {
             InitializeComponent();
+            //startGameUp();
+        }
+
+        public void startGameUp()
+        {
+            playerBlock.Template = Resources["playerTemplate"] as ControlTemplate;
+            playArea.Children.Add(playerBlock);
+            Canvas.SetBottom(playerBlock, 10);
+            Canvas.SetLeft(playerBlock, (playArea.ActualWidth / 2) - 25);
+            ball1.Template = Resources["ballTemplate"] as ControlTemplate;
+            playArea.Children.Add(ball1);
+            Canvas.SetBottom(ball1, (playArea.ActualHeight/2));
+            Canvas.SetLeft(ball1, (playArea.ActualWidth / 2) - 7.5);
+            
+            startButton.Visibility = Visibility.Hidden;
+            mainLoop.Interval = TimeSpan.FromMilliseconds(20); //50 per second
+            mainLoop.Tick += mainLoop_Tick;
+            mainLoop.Start();
+        }
+
+        void mainLoop_Tick(object sender, EventArgs e)//bliver kørt 50 gange i sekundet
+        {
+            //player controls
+            if (Keyboard.IsKeyDown(Key.Left))
+            {
+                Canvas.SetLeft(playerBlock, (Canvas.GetLeft(playerBlock) - 5));//5 er playerens hastighed
+            }
+            if (Keyboard.IsKeyDown(Key.Right))
+            {
+                Canvas.SetLeft(playerBlock, (Canvas.GetLeft(playerBlock) + 5));
+            }
+            //ball controls
+            Canvas.SetBottom(ball1, (Canvas.GetBottom(ball1)-3));
+            Canvas.SetLeft(ball1, (Canvas.GetLeft(ball1) + 3));
+            /*
+             hvis bold rammer side, hvis bold rammer player, sæt x speed / y speed til "xspeed *= -1", 
+             "3" skal udregnes ud fra en variabel der hedder speed
+             */
+        }
+
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            startGameUp();
         }
     }
 }
