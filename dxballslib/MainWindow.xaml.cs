c0782d598867de4c8e95a645ce5138be8c45e64f
Jesperb21 <Jesper.j.b.baunsgaard@gmail.com>
2014-02-28 10:47:31 +0100

    added so that the ball bounces when it hits the walls/roof when ball hits player it tests where it hits and changes directions/speeds depending on that. added variables for speeds.

--- dxballslib/MainWindow.xaml.cs
+++ dxballslib/MainWindow.xaml.cs
@@ -25,6 +25,11 @@ namespace dxballslib
         private DispatcherTimer mainLoop = new DispatcherTimer();
         private ContentControl playerBlock = new ContentControl();
         private ContentControl ball1 = new ContentControl();
+        private int xdirection = 1;
+        private int ydirection = 1;
+        private double ballspeed = 6;
+        private double xspeed;
+        private double yspeed;
         public MainWindow()
         {
             InitializeComponent();
@@ -41,8 +46,10 @@ namespace dxballslib
             playArea.Children.Add(ball1);
             Canvas.SetBottom(ball1, (playArea.ActualHeight/2));
             Canvas.SetLeft(ball1, (playArea.ActualWidth / 2) - 7.5);
-            
+            xspeed = ballspeed / 2;
+            yspeed = ballspeed / 2;
             startButton.Visibility = Visibility.Hidden;
+            
             mainLoop.Interval = TimeSpan.FromMilliseconds(20); //50 per second
             mainLoop.Tick += mainLoop_Tick;
             mainLoop.Start();
@@ -60,12 +67,54 @@ namespace dxballslib
                 Canvas.SetLeft(playerBlock, (Canvas.GetLeft(playerBlock) + 5));
             }
             //ball controls
-            Canvas.SetBottom(ball1, (Canvas.GetBottom(ball1)-3));
-            Canvas.SetLeft(ball1, (Canvas.GetLeft(ball1) + 3));
-            /*
-             hvis bold rammer side, hvis bold rammer player, sæt x speed / y speed til "xspeed *= -1", 
-             "3" skal udregnes ud fra en variabel der hedder speed
-             */
+            if (Canvas.GetLeft(ball1) >= (playArea.ActualWidth-ball1.ActualWidth))
+            {
+                xdirection = -1;
+            }
+            if(Canvas.GetLeft(ball1) <= 0)
+            {
+                xdirection = 1;
+            }
+            if (Canvas.GetBottom(ball1) <= 0)//ball hits buttom
+            {
+                ydirection = -1;
+            }
+            if (Canvas.GetBottom(ball1) >= playArea.ActualHeight)
+            {
+                ydirection = 1;
+            }
+            if (Canvas.GetBottom(ball1) <= 10 + playerBlock.ActualHeight && Canvas.GetBottom(ball1) >= 10)//ball player hittest
+            {
+                if((Canvas.GetLeft(ball1) >= Canvas.GetLeft(playerBlock)) && (Canvas.GetLeft(ball1) <= Canvas.GetLeft(playerBlock)+playerBlock.ActualWidth))
+                {
+                    double v1 = ((Canvas.GetLeft(ball1) + ball1.ActualWidth / 2) - Canvas.GetLeft(playerBlock));
+                    double v2 = ((Canvas.GetLeft(playerBlock) + playerBlock.ActualWidth / 2) - Canvas.GetLeft(playerBlock));
+                    v1 -= v2;
+                    xspeed = v1 / v2;
+                    xspeed *= ballspeed;
+                    if (xspeed < 0)
+                    {//left side of the player block
+                        xspeed = -xspeed;
+                        xdirection = -1;
+                    }
+                    else
+                    {
+                        xdirection = 1;
+                    }
+                    if (xspeed > ballspeed - 0.5)
+                    {
+                        xspeed = ballspeed-0.5;
+                    }
+                    yspeed = ballspeed - xspeed;
+                    if (yspeed < 0)
+                    {
+                        yspeed = -yspeed;
+                    }
+                    ydirection = -1;
+                }
+            }
+            Canvas.SetBottom(ball1, (Canvas.GetBottom(ball1) - (yspeed * ydirection)));
+            Canvas.SetLeft(ball1, (Canvas.GetLeft(ball1) + (xspeed*xdirection)));
         }
 
 
