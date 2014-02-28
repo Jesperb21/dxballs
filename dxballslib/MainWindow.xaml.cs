7cc0c80f47eea52bd3d8e26bc83b1dab1a31bac2
Jesperb21 <Jesper.j.b.baunsgaard@gmail.com>
2014-02-28 19:59:14 +0100

    added a few comments

--- dxballslib/MainWindow.xaml.cs
+++ dxballslib/MainWindow.xaml.cs
@@ -22,62 +22,71 @@ namespace dxballslib
     /// </summary>
     public partial class MainWindow : Window
     {
-        private DispatcherTimer mainLoop = new DispatcherTimer();
-        private ContentControl playerBlock = new ContentControl();
-        private ContentControl ball1 = new ContentControl();
-        private int xdirection = 1;
-        private int ydirection = 1;
-        private double ballspeed = 6;
-        private double xspeed;
-        private double yspeed;
-        public List<ContentControl> enemyBlockList = new List<ContentControl>();
-        public double enemyMovementSpeed = 0.1;
-        public int spawnInterval = 250;
-        private int spawnCounter;
-        public int score;
+        public List<ContentControl> ballList = new List<ContentControl>(); //list of the ball objects in the game at the moment
+        public List<ContentControl> enemyBlockList = new List<ContentControl>(); //list of enemyblocks in the game at the moment
+        public double enemyMovementSpeed = 0.1; //enemyBlocks descending speed
+        public int spawnInterval = 250;//spawn interval for a new line of enemyBlocks, measured in ticks from the mainLoop
+        public int score; //the score
+        public int playerMovementSpeed = 5;
+
+        private DispatcherTimer mainLoop = new DispatcherTimer();//main game loop
+        private ContentControl playerBlock = new ContentControl();//the player
+        private ContentControl ball1 = new ContentControl(); // -----------------------to be removed
+        private int xdirection = 1; // ball variables
+        private int ydirection = 1;// ball variables
+        private double ballspeed = 6;// ball variables
+        private double xspeed;// ball variables
+        private double yspeed;// ball variables
+        private int spawnCounter; //counter for spawnInterval
+
+        //nothing to see here
         public MainWindow()
         {
             InitializeComponent();
-            //startGameUp();
-        }
+        } 
 
+        //the main startup function, add a difficulty parameter etc here
         public void startGameUp()
         {
-            score = 0;
+            score = 0; //set the score to 0
+
+            //place a player on the screen in the middle
             playerBlock.Template = Resources["playerTemplate"] as ControlTemplate;
             playArea.Children.Add(playerBlock);
             Canvas.SetBottom(playerBlock, 10);
             Canvas.SetLeft(playerBlock, (playArea.ActualWidth / 2) - 25);
+            //place a ball on the screen in the middle of the screen
             ball1.Template = Resources["ballTemplate"] as ControlTemplate;
             playArea.Children.Add(ball1);
             Canvas.SetBottom(ball1, (playArea.ActualHeight/2));
             Canvas.SetLeft(ball1, (playArea.ActualWidth / 2) - 7.5);
+            //set ball parameters
             xspeed = ballspeed / 2;
             yspeed = ballspeed / 2;
-            startButton.Visibility = Visibility.Hidden;
-            spawnCounter = spawnInterval;
+            startButton.Visibility = Visibility.Hidden; //hide the start game button
+            spawnCounter = spawnInterval; // spawn a new line of enemies on the first tick of the game
             mainLoop.Interval = TimeSpan.FromMilliseconds(20); //50 per second
-            mainLoop.Tick += mainLoop_Tick;
-            mainLoop.Start();
+            mainLoop.Tick += mainLoop_Tick; //add an event handler for the "mainLoop.Tick" event
+            mainLoop.Start();//start the dispatchertimer
         }
 
         void mainLoop_Tick(object sender, EventArgs e)//bliver kørt 50 gange i sekundet
         {
             //player controls
-            if (Keyboard.IsKeyDown(Key.Left))
+            if (Keyboard.IsKeyDown(Key.Left) || (Keyboard.IsKeyDown(Key.A)))//move left
             {
-                Canvas.SetLeft(playerBlock, (Canvas.GetLeft(playerBlock) - 5));//5 er playerens hastighed
+                Canvas.SetLeft(playerBlock, (Canvas.GetLeft(playerBlock) - playerMovementSpeed));
             }
-            if (Keyboard.IsKeyDown(Key.Right))
+            if (Keyboard.IsKeyDown(Key.Right) || (Keyboard.IsKeyDown(Key.D)))//move right
             {
-                Canvas.SetLeft(playerBlock, (Canvas.GetLeft(playerBlock) + 5));
+                Canvas.SetLeft(playerBlock, (Canvas.GetLeft(playerBlock) + playerMovementSpeed));
             }
             //ball controls
-            if (Canvas.GetLeft(ball1) >= (playArea.ActualWidth-ball1.ActualWidth))
+            if (Canvas.GetLeft(ball1) >= (playArea.ActualWidth-ball1.ActualWidth))//hvis bolden rammer højre side
             {
                 xdirection = -1;
             }
-            if(Canvas.GetLeft(ball1) <= 0)
+            if(Canvas.GetLeft(ball1) <= 0)// hvis bolden rammer venstre side
             {
                 xdirection = 1;
             }
@@ -85,107 +94,110 @@ namespace dxballslib
             {
                 ydirection = -1;
             }
-            if (Canvas.GetBottom(ball1) >= playArea.ActualHeight)
+            if (Canvas.GetBottom(ball1) >= playArea.ActualHeight)//hvis bolden rammer toppen
             {
                 ydirection = 1;
             }
+            
             if (Canvas.GetBottom(ball1) <= 10 + playerBlock.ActualHeight && Canvas.GetBottom(ball1) >= 10)//ball player hittest
             {
                 if((Canvas.GetLeft(ball1) >= Canvas.GetLeft(playerBlock)) && (Canvas.GetLeft(ball1) <= Canvas.GetLeft(playerBlock)+playerBlock.ActualWidth))
                 {
+                    //find ud af hvor bolden rammer playerblocken 
                     double v1 = ((Canvas.GetLeft(ball1) + ball1.ActualWidth / 2) - Canvas.GetLeft(playerBlock));
                     double v2 = ((Canvas.GetLeft(playerBlock) + playerBlock.ActualWidth / 2) - Canvas.GetLeft(playerBlock));
                     v1 -= v2;
                     xspeed = v1 / v2;
-                    xspeed *= ballspeed;
-                    if (xspeed < 0)
-                    {//left side of the player block
+                    xspeed *= ballspeed; //gang forholdet med boldens hastighed
+                    if (xspeed < 0) //hvis bolden ramte playerblockens venstre side
+                    {
                         xspeed = -xspeed;
-                        xdirection = -1;
+                        xdirection = -1;//få bolden til at bevæge sig mod venstre
                     }
                     else
-                    {
+                    {//ellers bevæg den mod højre
                         xdirection = 1;
                     }
-                    if (xspeed > ballspeed - 0.5)
+                    if (xspeed > ballspeed - 0.5)//hvis bolden bevæger sig for hurtigt vandret(og ikke efterlader noget af boldens max hastighed til y-hastigheden)
                     {
-                        xspeed = ballspeed-0.5;
+                        xspeed = ballspeed-0.5;//så sænk dens x-hastighed til et maximum, 0.5 mindre end boldens totale max hastighed
                     }
-                    yspeed = ballspeed - xspeed;
-                    if (yspeed < 0)
+                    yspeed = ballspeed - xspeed;//y hastighed udregnes ud fra boldens max hastighed og dens x hastighed
+                    if (yspeed < 0) //dont go negative
                     {
                         yspeed = -yspeed;
                     }
-                    ydirection = -1;
+
+                    ydirection = -1; //bevæg bolden opad nu
                 }
             }
-            for (int i = 0; i < enemyBlockList.Count(); i++ )
+            for (int i = 0; i < enemyBlockList.Count(); i++ ) //loop igennem hver enemyBlock i enemyBlockList'en
             {
                 double ballTop = playArea.ActualHeight - (Canvas.GetBottom(ball1) + ball1.ActualHeight); //boldenes position bliver sat via "SetBottom" 
                 //så de har ikke nogen "getTop" der kunne bruges, så udregn hvad dens "top" ville være.
                 if (ballTop >= Canvas.GetTop(enemyBlockList[i]) && ballTop <= (Canvas.GetTop(enemyBlockList[i]) + enemyBlockList[i].ActualHeight))
                 {//dens top er indenfor enemy'ens "top plus dens højde" område.
                     if (Canvas.GetLeft(ball1) >= Canvas.GetLeft(enemyBlockList[i]) && (Canvas.GetLeft(ball1) + ball1.ActualWidth) <= (Canvas.GetLeft(enemyBlockList[i]) + enemyBlockList[i].ActualWidth))
-                    {
+                    {//dens left er indefor enemy'ens "left plus dens bredde" område
                         if (ydirection == 1)
-                        {
+                        {//bevæg dig op hvis du er på vej ned
                             ydirection = -1;
                         }
                         else
-                        {
+                        {//ellers gå nedad
                             ydirection = 1;
                         }
 
-                        playArea.Children.Remove(enemyBlockList[i]);
-                        enemyBlockList.RemoveAt(i);
-                        score += 100;
+                        playArea.Children.Remove(enemyBlockList[i]);//fjern enemyblock'en fra skærmen
+                        enemyBlockList.RemoveAt(i);//og fra listen
+                        score += 100;//tilføj lidt score
                     }
                 }
             }
-            Canvas.SetBottom(ball1, (Canvas.GetBottom(ball1) - (yspeed * ydirection)));
-            Canvas.SetLeft(ball1, (Canvas.GetLeft(ball1) + (xspeed*xdirection)));
+            Canvas.SetBottom(ball1, (Canvas.GetBottom(ball1) - (yspeed * ydirection)));//flyt bolden
+            Canvas.SetLeft(ball1, (Canvas.GetLeft(ball1) + (xspeed*xdirection)));//flyt bolden
+            
             //enemy movement downwards
-            foreach (ContentControl enemy in enemyBlockList)
+            foreach (ContentControl enemy in enemyBlockList)//for all enemies
             {
-                Canvas.SetTop(enemy, Canvas.GetTop(enemy) + enemyMovementSpeed);
+                Canvas.SetTop(enemy, Canvas.GetTop(enemy) + enemyMovementSpeed);//move a bit downward, depending on their movementspeed
             }
             if (spawnCounter >= spawnInterval)
-            {
+            {//spawn some enemies hvis du har ventet nok ticks
                 addEnemyLine();
-                spawnCounter = 0;
+                spawnCounter = 0; //reset spawncounteren
             }
-            spawnCounter++;
+            spawnCounter++;//another tick went by
             ScoreText.Text = score.ToString(); //update the score textblock
         }
 
 
         private void startButton_Click(object sender, RoutedEventArgs e)
         {
-            startGameUp();
+            startGameUp();//start game
         }
         //added from GeneratedCode\main.cs
-        private void addEnemy(Point spawnLocation)
+        private void addEnemy(Point spawnLocation)//spawn enemy on location specified in the spawnLocation
         {
             ContentControl temp = new ContentControl();
             temp.Template = Resources["enemyStandardBlock"] as ControlTemplate;
             playArea.Children.Add(temp);
             Canvas.SetTop(temp, spawnLocation.Y);
             Canvas.SetLeft(temp, spawnLocation.X);
-            enemyBlockList.Add(temp);
-            //MessageBox.Show("derp");
+            enemyBlockList.Add(temp);//add it to the array
         }
-        public virtual void addEnemyLine()
+        public virtual void addEnemyLine()//spawn a line of enemies
         {
-            int numberOfBlocksToAdd = 10;
-            int widthPerBlock = 50;
-            int spaceBetweenBlocks = 5;
-            int totalWidthOfBlocksWhenPlaced = (numberOfBlocksToAdd * widthPerBlock) + ((numberOfBlocksToAdd - 1) * spaceBetweenBlocks);
-            if (playArea.ActualWidth < (totalWidthOfBlocksWhenPlaced+16))
+            int numberOfBlocksToAdd = 10;//Enemies in a line
+            int widthPerBlock = 50;//width of each enemy
+            int spaceBetweenBlocks = 5;//space between enemies
+            int totalWidthOfBlocksWhenPlaced = (numberOfBlocksToAdd * widthPerBlock) + ((numberOfBlocksToAdd - 1) * spaceBetweenBlocks);//total required for the enemy "field"
+            if (playArea.ActualWidth < (totalWidthOfBlocksWhenPlaced+16)) //if screen is too small to fit all enemies resize screen
             {
                 Application.Current.MainWindow.Width = totalWidthOfBlocksWhenPlaced+16;//ved ikke hvorfor +16, men ellers så gav den en spaceToFirstBlock på -8 / -4
             }
-            int spaceToFirstBlock = ((int)playArea.ActualWidth - totalWidthOfBlocksWhenPlaced)/2;
-            for(int i = 0; i <numberOfBlocksToAdd; i++){
+            int spaceToFirstBlock = ((int)playArea.ActualWidth - totalWidthOfBlocksWhenPlaced)/2; //space to move in before the first enemy block can be spawned (so all will be at the center)
+            for(int i = 0; i <numberOfBlocksToAdd; i++){//spawn enemies
                 addEnemy(new Point((i*55)+spaceToFirstBlock, 0));   
             }
         }
